c684c312caa7aaacac3f8a0b48168afcae509c24
Chun Yang <yang2007chun@gmail.com>
2016-06-30 13:01:48 -0500

    move tags-to-component-names to a plugin

--- test/unit/lib/plugins/tags-to-component-names/process-each-jsx-snippet.js
+++ test/unit/lib/plugins/tags-to-component-names/process-each-jsx-snippet.js
@@ -1,13 +1,16 @@
 /* eslint-env mocha */
 var assert = require('chai').assert
-var parse = require('../../../../lib/helpers/parse')
+var parse = require('../../../../../lib/helpers/parse')
 var generate = require('babel-generator').default
-var tagsToComponentNames = require('../../../../lib/html-snippet-to-jsx-snippet/tags-to-component-names')
+var tagsToComponentNames = require('../../../../../lib/plugins/tags-to-component-names/process-each-jsx-snippet')
 
 describe('change tags to component names', function () {
   it('should change tags to component names', function () {
-    var ast = parse('<div><div data-component-name="ComponentA"/></div>')
-    tagsToComponentNames({ast: ast})
+    const { component: { ast } } = tagsToComponentNames({
+      component: {
+        ast: parse('<div><div data-component-name="ComponentA"/></div>')
+      }
+    })
 
     assert.deepEqual(
       generate(ast, {}, '').code,

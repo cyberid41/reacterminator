00feee97e05963e86d214fdff696699bbb7d41ee
Kristy Miller <miller.kristy06@gmail.com>
2016-07-08 11:10:34 -0500

    Fix reducer content test and then update file to make test pass

--- test/integration/redux-integration.js
+++ test/integration/redux-integration.js
@@ -252,9 +252,10 @@ export default customize(actionCreator, { actionTypeConstants });
       fs.readFileSync('./reacterminator/reducers/redux-example/name.js', 'utf8'),
       `\
 /* eslint-disable */
+import custom from '../../../custom/index';
 import actionTypeConstants from '../../action-type-constants/index';
 
-export default function name(state = '', action) {
+const reducer = name(state = '', action) => {
   switch (action.type) {
     case actionTypeConstants.reduxExample.changeName:
       return action.value;
@@ -262,6 +263,10 @@ export default function name(state = '', action) {
       return state;
   }
 }
+
+const customize = custom['reducers/redux-example/name'] || ((x) => x);
+
+export default customize(reducer, { actionTypeConstants });
 `
     )
 

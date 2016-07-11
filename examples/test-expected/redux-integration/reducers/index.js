09d6a77b51d03ae50e5226c987cc770672db1977
Kristy Miller <miller.kristy06@gmail.com>
2016-07-11 13:38:08 -0500

    Update test example for reducers index to match new structure

--- examples/test-expected/redux-integration/reducers/index.js
+++ examples/test-expected/redux-integration/reducers/index.js
@@ -1,14 +1,33 @@
 /* eslint-disable */
-import { combineReducers } from 'redux';
 import reduxExample from './redux-example/index';
+import { combineReducers } from 'redux';
+import custom from '../../custom/index';
+import helpers from '../helpers/index';
 
-export default (state = {}, action) => {
+const baseReducers = {
+  reduxExample
+};
+
+const additionalReducers = helpers.getAdditional({
+  type: 'reducers',
+  path: '',
+  baseFiles: [
+    'reduxExample',
+  ],
+  custom,
+});
+
+const reducers = Object.assign(baseReducers, additionalReducers);
+
+const reducer = (state, action) => {
   switch (action.type) {
     case 'REDUCERS_INITIALIZE':
       return action.value;
     default:
-      return combineReducers({
-reduxExample
-      })(state, action);
+      return combineReducers(reducers)(state, action);
   }
 }
+
+const customize = custom['reducers/index'] || ((x) => x);
+
+export default customize(reducer, reducers);

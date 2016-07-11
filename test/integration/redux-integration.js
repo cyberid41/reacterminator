087033e87f2268b81af3e0206edd7edaa7f68aeb
Kristy Miller <miller.kristy06@gmail.com>
2016-07-11 08:54:20 -0500

    Update redux integration test file to pass ESLint

--- test/integration/redux-integration.js
+++ test/integration/redux-integration.js
@@ -1,38 +1,38 @@
 /* eslint-env mocha */
-const fs = require('fs')
-const shell = require('shelljs')
-const assert = require('chai').assert
-const reacterminator = require('../../lib/index')
+const fs = require('fs');
+const shell = require('shelljs');
+const assert = require('chai').assert;
+const reacterminator = require('../../lib/index');
 
-function checkFile (filePath) {
+function checkFile(filePath) {
   assert.deepEqual(
     fs.readFileSync(`./reacterminator/${filePath}`, 'utf8'),
     fs.readFileSync(`./examples/test-expected/redux-integration/${filePath}`, 'utf8')
-  )
+  );
 }
 
-function processFile (filePath) {
+function processFile(filePath) {
   reacterminator(
     {
       type: 'path',
-      content: filePath
+      content: filePath,
     },
     {
       fileToComponent: true,
-      generateFiles: true
+      generateFiles: true,
     }
-  )
+  );
 }
 
-describe('redux-integration', function () {
+describe('redux-integration', () => {
   beforeEach(function () {
-    shell.rm('-rf', './reacterminator')
-  })
+    shell.rm('-rf', './reacterminator');
+  });
 
-  it('should hook redux into component and generate redux files', function () {
-    processFile('./examples/test/redux-example.html')
+  it('should hook redux into component and generate redux files', () => {
+    processFile('./examples/test/redux-example.html');
 
-    const filePaths= [
+    const filePaths = [
       'components/ReduxExample.jsx',
       'components/ComponentA.jsx',
       'store.js',
@@ -44,16 +44,15 @@ describe('redux-integration', function () {
       'reducers/redux-example/name.js',
       'reducers/redux-example/is-going.js',
       'reducers/redux-example/index.js',
-      'reducers/index.js'
-    ]
+      'reducers/index.js',
+    ];
 
-    filePaths.forEach(checkFile)
+    filePaths.forEach(checkFile);
+  });
 
-  })
+  it('should not create reducer file for a path if there is no reducers to combile', () => {
+    processFile('./examples/test/redux-no-reducers.html');
 
-  it('should not create reducer file for a path if there is no reducers to combile', function () {
-    processFile('./examples/test/redux-no-reducers.html')
-
-    checkFile('reducers/redux-no-reducers/index.js')
-  })
-})
+    checkFile('reducers/redux-no-reducers/index.js');
+  });
+});

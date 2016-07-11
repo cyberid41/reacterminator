e1e19aba7c03e7669e7323193c9433cebb4c7004
Kristy Miller <miller.kristy06@gmail.com>
2016-07-11 09:15:45 -0500

    Update redux component index test to new structure checking for custom reducers

--- examples/test-expected/redux-integration/reducers/redux-example/index.js
+++ examples/test-expected/redux-integration/reducers/redux-example/index.js
@@ -3,16 +3,37 @@ import { combineReducers } from 'redux';
 import isGoing from './is-going';
 import name from './name';
 import phoneNumber from './phone-number';
+import custom from '../../../custom/index';
+import helpers from '../../helpers/index';
 
-export default (state = {}, action) => {
+const baseReducers = {
+  isGoing,
+  name,
+  phoneNumber,
+};
+
+const additionalReducers = helpers.getAdditional({
+  type: 'reducers',
+  path: 'example-reduces',
+  baseFiles: [
+    'isGoing',
+    'name',
+    'phoneNumber',
+  ],
+  custom,
+});
+
+const reducers = Object.assign(baseReducers, additionalReducers);
+
+const reducer = (state, action) => {
   switch (action.type) {
     case 'REDUX_EXAMPLE_INITIALIZE':
       return action.value;
     default:
-      return combineReducers({
-isGoing,
-  name,
-  phoneNumber
-      })(state, action);
+      return combineReducers(reducers)(state, action);
   }
 }
+
+const customize = custom['reducers/redux-example/index'] || ((x) => x);
+
+export default customize(reducer, reducers)

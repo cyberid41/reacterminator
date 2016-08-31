01b8a70dd7d0e3a7b39ad2977b4ec03dff8e0f6a
Chun-Yang <yang2007chun@gmail.com>
2016-08-31 12:01:50 -0500

    use composition instead of inheritance

--- examples/test-expected/generate/custom/components/Unicorn.jsx
+++ examples/test-expected/generate/custom/components/Unicorn.jsx
@@ -1,7 +1,9 @@
-export default (Base) => {
-  class Unicorn extends Base {
-    componentWillMount() {
+import React from 'react';
 
+export default (Base) => {
+  class Unicorn extends React.Component {
+    render () {
+      return <Base/>
     }
   }
 
